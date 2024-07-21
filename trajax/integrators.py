3fc2ada24a4933e2c399dcfc69f4fdb420b71ec6
Karen Leung <karen.ym.leung@gmail.com>
2024-07-21 15:21:04 -0700

    minor big fixes

--- trajax/integrators.py
+++ trajax/integrators.py
@@ -23,8 +23,8 @@ def rk4(dynamics, dt=0.01):
   def integrator(x, u, t):
     dt2 = dt / 2.0
     k1 = dynamics(x, u, t)
-    k2 = dynamics(x + dt2 * k1, u, t)
-    k3 = dynamics(x + dt2 * k2, u, t)
-    k4 = dynamics(x + dt * k3, u, t)
+    k2 = dynamics(x + dt2 * k1, u, t + dt2)
+    k3 = dynamics(x + dt2 * k2, u, t + dt2)
+    k4 = dynamics(x + dt * k3, u, t + dt)
     return x + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
   return integrator
